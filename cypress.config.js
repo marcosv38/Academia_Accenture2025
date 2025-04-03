cf25bebc2b0b7bcdf6303ec26ce3cafc4a6d8db5
marcos <marcos.bwr@gmail.com>
2025-04-03 10:35:54 -0300

    Configuração incial do projeto

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  env: {
+    emailQuote: '',
+    passwordQuote: '',
+  },
+  e2e: {
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    watchForFileChanges: false,
+    specPattern: 'cypress/e2e/**/*.feature',
+    baseUrl: 'https://sampleapp.tricentis.com/101/app.php',
+
+    setupNodeEvents(on, config) {
+      const cucumber = require('cypress-cucumber-preprocessor').default;
+      on('file:preprocessor', cucumber());
+    },
+  },
+});
